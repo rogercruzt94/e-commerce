296ffb8fc84c00d5e86cb857a234cef4aa636972
rogercruzt94 <101655457+rogercruzt94@users.noreply.github.com>
2022-07-18 22:56:15 -0500

    se creo navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    <div>
+
+
+        <button className='Navbar__button'>Menú</button>
+        <button className='Navbar__button'>Productos</button>
+        <button className='Navbar__button'>Sobre Nosotros</button>
+        <button className='Navbar__button'>Preguntas Frecuentes</button>
+
+        <h1>E-COMMERCE ROGER CRUZ</h1>
+        <h2>Segunda entrega Menu e-commerce</h2>
+
+
+    </div>
+  )
+}
+
+export default Navbar
